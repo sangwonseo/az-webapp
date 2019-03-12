ecc19658f56e8a616fd8f759cd8ccc7494f1a083
sangwonseo <sangwon.seo@netapp.com>
2019-03-12 04:14:50 +0000

    root 뷰 구현

--- sangwon/urls.py
+++ sangwon/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.http import HttpResponse
+def root(request):
+    return HttpResponse('Hello World')
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', root),
 ]
