c75fd6e016629c4052ec78bcfbf6910b6923edcb
sangwonseo <sangwon.seo@netapp.com>
2019-03-12 03:53:28 +0000

    updated

--- sangwon/settings.py
+++ sangwon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kns(tfatw%qx%%ul0+adu4#^v@q+d^w!y(mdap=axluf4=%pjz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".goorm.io"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
